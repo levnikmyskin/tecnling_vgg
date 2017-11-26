c535179bb91e0b6528ca8fd7c934c9f140445948
alessio <alessiomolinar@gmail.com>
2017-11-26 01:53:05 +0100

    added mongodb helper class

--- retoricaweb/src/main/java/unipi/tecnling/retoricaweb/HelloWorld.java
+++ retoricaweb/src/main/java/unipi/tecnling/retoricaweb/HelloWorld.java
@@ -1,16 +1,22 @@
 package unipi.tecnling.retoricaweb;
 
-import javax.faces.bean.ManagedBean;
+import com.mongodb.MongoClient;
+import unipi.tecnling.retoricaweb.utils.MongodbHelper;
 
+import javax.faces.bean.ManagedBean;
 
 @ManagedBean(name = "helloWorld", eager = true)
 public class HelloWorld {
 
     public HelloWorld() {
+        MongodbHelper mdbHelper = new MongodbHelper();
+        MongoClient instance = mdbHelper.connect();
+
         System.out.println("HelloWorld started!");
     }
 
     public String getMessage() {
+        System.out.println("prova");
         return "Hello heaven!";
     }
 }
---
+++ retoricaweb/src/main/java/unipi/tecnling/retoricaweb/utils/MongodbHelper.java
@@ -0,0 +1,68 @@
+package unipi.tecnling.retoricaweb.utils;
+
+import com.mongodb.MongoClient;
+import com.mongodb.ServerAddress;
+import com.mongodb.MongoCredential;
+import com.mongodb.MongoClientOptions;
+
+import javax.management.RuntimeErrorException;
+import java.io.*;
+import java.net.URL;
+import java.util.Collections;
+import java.util.Properties;
+
+/*
+ * @author Alessio
+ *
+ * Classe d'aiuto per la connessione e l'interazione con MongoDb.
+ * In tutta l'applicazione dovrebbe essere usata la stessa istanza
+ * ritornata dal metodo connect()
+ */
+
+public class MongodbHelper {
+
+    public MongoClient connect(){
+        String[] configs = getConfigs();
+        String user = configs[0];
+        String db = configs[1];
+        char[] pwd = configs[2].toCharArray();  // password needs to be a char array
+        String host = configs[3];
+        int port = Integer.parseInt(configs[4]);  // ServerAddress port must be an int
+
+        MongoCredential credentials = MongoCredential.createCredential(user, db, pwd);
+        MongoClientOptions options = MongoClientOptions.builder().sslEnabled(true).build();
+
+        return new MongoClient(new ServerAddress(host, port),
+                Collections.singletonList(credentials),
+                options);
+    }
+
+    private String[] getConfigs(){
+        String[] configs = new String[5];
+        Properties properties = new Properties();
+        InputStream input;
+        try {
+            // get relative path of file and throw exception if it's null
+            String path = MongodbHelper.class.getClassLoader().getResource("config.properties").getFile();
+            if (path == null){
+                throw new IOException();
+            }
+
+            File file = new File(path);
+
+            input = new FileInputStream(file);
+            properties.load(input);
+            configs[0] = properties.getProperty("user");
+            configs[1] = properties.getProperty("database");
+            configs[2] = properties.getProperty("passwd");
+            configs[3] = properties.getProperty("host");
+            configs[4] = properties.getProperty("port");
+
+            input.close();
+            return configs;
+        } catch (IOException e){
+            throw new RuntimeException("Could not parse config.properties file. Please check it is available in" +
+                    " the utils folder");
+        }
+    }
+}
