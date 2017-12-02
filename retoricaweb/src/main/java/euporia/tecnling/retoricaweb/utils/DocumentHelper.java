53044cbe42dc06253b235137cfa82afc623e1689
Andreapdr <andrea.pdr@hotmail.it>
2017-12-02 19:34:20 +0100

    added comments

--- retoricaweb/src/main/java/euporia/tecnling/retoricaweb/utils/DocumentHelper.java
+++ retoricaweb/src/main/java/euporia/tecnling/retoricaweb/utils/DocumentHelper.java
@@ -8,6 +8,14 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.StringTokenizer;
 
+/**
+ *  Helper class to manage document upload and store it into database
+ *
+ *  @author andrea
+ *  @author alessio
+ */
+
+// TODO to be complited
 public class DocumentHelper {
 
     public static boolean saveDocument(String text){
@@ -16,6 +24,16 @@ public class DocumentHelper {
 
     }
 
+    /**
+     * It tokenizes text and returns document's instance related to text lines.
+     * document structure:
+     * Document lines: Document subLines{
+     *     1: [word0, word1, .. wordN],
+     *     2: [word0, word1, .. wordN],
+     *     3: ....
+     * }
+     */
+
     private static Document getLines(String text){
         try{
             BufferedReader br = new BufferedReader(new FileReader(text));
@@ -23,15 +41,17 @@ public class DocumentHelper {
             Document subLines = new Document();
             int i = 0;
             while ((line = br.readLine()) != null){
-                int index = 0;
                 ArrayList<String> lineArray = new ArrayList<String>();
+
+                // Sets ", '" as delimiters to the tokenization
                 StringTokenizer stringTokenizer = new StringTokenizer(line, ", '");
 
+                // Appends tokens to list lineArray
                 while (stringTokenizer.hasMoreTokens()){
                     lineArray.add(stringTokenizer.nextToken());
-                    index++;
                 }
 
+                // Appends to Document subLine key: line number, value: line words
                 subLines.append(String.valueOf(i), lineArray);
                 i++;
             }
